8a1fd4c3a6a2f63378700bcb9afbeee037603991
Francesco Pirrotta <fpirrotta@topcon.com>
2017-05-03 12:54:45 +0200

    Update declaration

--- declarations/ninjagoat-testing.d.ts
+++ declarations/ninjagoat-testing.d.ts
@@ -1,4 +1,7 @@
-import { ViewModelContext } from "ninjagoat";
+import { interfaces } from "inversify";
+import { CommandEnvelope } from "ninjagoat-commands";
+import { IModelRetriever, ModelState, ModelRetriever } from "ninjagoat-projections";
+import { ViewModelContext, Dictionary, IViewModelRegistry, IServiceLocator, IModule } from "ninjagoat";
 
 export interface IContextRegistry {
     register(context: ViewModelContext): IContextRegistry;
@@ -6,4 +9,25 @@ export interface IContextRegistry {
 
 export interface IContextRegistryChecker {
     exist(context: ViewModelContext): boolean;
+}
+
+export interface ICommandHandler {
+    handle(envelope: CommandEnvelope, context: ViewModelContext): void;
+}
+
+export class TestModule implements IModule {
+    modules(container: interfaces.Container): void;
+    register(registry: IViewModelRegistry, serviceLocator?: IServiceLocator, overrides?: any): void;
+}
+
+export class ContextRegistry implements IContextRegistry, IContextRegistryChecker {
+    public register(context: ViewModelContext): IContextRegistry;
+    public exist(context: ViewModelContext): boolean;
+}
+
+export class FileModelRetriever implements IModelRetriever, ICommandHandler {
+    constructor(modelRetriever: ModelRetriever, contextRegistryChecker: IContextRegistryChecker, mockFiles: Dictionary<Dictionary<any>>, scheduler?: Rx.Scheduler);
+
+    public modelFor<T>(context: ViewModelContext): Rx.Observable<ModelState<T>>;
+    public handle(envelope: CommandEnvelope, context: ViewModelContext): void;
 }
---
+++ scripts/ninjagoat-testing.ts
@@ -0,0 +1,3 @@
+export { default as TestModule } from "./TestModule";
+export { FileModelRetriever } from "./FileModelRetriever";
+export { ContextRegistry } from "./registry/ContextRegistry";
