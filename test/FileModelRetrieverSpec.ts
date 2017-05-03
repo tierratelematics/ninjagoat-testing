cb013ae48a18cabbbcb918c0e062a96a8c02fdf4
Francesco Pirrotta <fpirrotta@topcon.com>
2017-05-03 13:07:31 +0200

    Fix FileModelRetriever test

--- test/FileModelRetrieverSpec.ts
+++ test/FileModelRetrieverSpec.ts
@@ -17,7 +17,6 @@ describe("The FileModelRetriever", () => {
     let retriever: TypeMoq.IMock<IModelRetriever>;
     let _context: ViewModelContext;
     let invalidContext: ViewModelContext;
-    let command: CommandEnvelope;
     let files: Dictionary<Dictionary<any>>;
     let notifications: ModelState<any>[];
     let testScheduler: Rx.TestScheduler;
@@ -28,7 +27,6 @@ describe("The FileModelRetriever", () => {
         retriever = TypeMoq.Mock.ofType<IModelRetriever>();
         checker = TypeMoq.Mock.ofType<IContextRegistryChecker>();
         _context = { area: "anArea", viewmodelId: "anId", parameters: {} };
-        command = <CommandEnvelope>{ type: "aCommand" };
 
         invalidContext = { area: "anArea", viewmodelId: "anIdW/outMock", parameters: {} };
         files = { "anArea": { "anId": { "_": { "id": "baseModel" }, "aCommand": { "id": "commandModel" } } } };
@@ -93,41 +91,24 @@ describe("The FileModelRetriever", () => {
 
     });
 
-    context("when a command is handled", () => {
-        context("and the mock file for the command exists", () => {
-            it("should send immediately a loading model state follow by a ready model state with the mock file", () => {
-                subject.modelFor<any>(_context).subscribe(data => notifications.push(data));
+    context("when a model is pushed", () => {
+        it("should send immediately a loading model state follow by a ready model state with the mock file", () => {
+            subject.modelFor<any>(_context).subscribe(data => notifications.push(data));
 
-                testScheduler.advanceBy(2000);
-                subject.handle(command, _context);
+            testScheduler.advanceBy(2000);
+            subject.pushModel(files["anArea"]["anId"]["aCommand"], _context);
 
-                testScheduler.advanceBy(2000);
-                expect(notifications.length).to.be(4);
-                expect(notifications[2].phase).to.be(ModelPhase.Loading);
-                expect(notifications[3].phase).to.be(ModelPhase.Ready);
-                expect(notifications[3].model).to.be.eql(files["anArea"]["anId"]["aCommand"]);
-            });
+            testScheduler.advanceBy(2000);
+            expect(notifications.length).to.be(4);
+            expect(notifications[2].phase).to.be(ModelPhase.Loading);
+            expect(notifications[3].phase).to.be(ModelPhase.Ready);
+            expect(notifications[3].model).to.be.eql(files["anArea"]["anId"]["aCommand"]);
         });
+    });
 
-        context("but the mock file for the command do not exists", () => {
-            it("should send immediately a loading model state follow by a failed model state", () => {
-                subject.modelFor<any>(_context).subscribe(data => notifications.push(data));
-
-                testScheduler.advanceBy(2000);
-                command.type = "anInvalidCommand";
-                subject.handle(command, _context);
-
-                testScheduler.advanceBy(2000);
-                expect(notifications.length).to.be(4);
-                expect(notifications[2].phase).to.be(ModelPhase.Loading);
-                expect(notifications[3].phase).to.be(ModelPhase.Failed);
-            });
-        });
-
-        context("but the was never retrieved the model", () => {
-            it("should throw an error", () => {
-                expect(() => subject.handle(command, invalidContext)).to.throwError();
-            });
+    context("but the model was never retrieved", () => {
+        it("should throw an error", () => {
+            expect(() => subject.pushModel(files["anArea"]["anId"]["aCommand"], invalidContext)).to.throwError();
         });
     });
 });
