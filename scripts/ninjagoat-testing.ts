248a45696cbfa451df9994c55b661b713f32c588
Francesco Pirrotta <fpirrotta@topcon.com>
2017-05-03 15:52:11 +0200

    Export FileModelResolver

--- scripts/ninjagoat-testing.ts
+++ scripts/ninjagoat-testing.ts
@@ -1,3 +1,4 @@
 export { default as TestModule } from "./TestModule";
 export { FileModelRetriever } from "./FileModelRetriever";
 export { ContextRegistry } from "./registry/ContextRegistry";
+export { FileModelResolver } from "./resolver/FileModelResolver";
