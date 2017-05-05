54e5c21b2393a09e9a3971e646eca9a6be80c4b0
Enrico Calzavara <ecalzavara@tierratelematics.com>
2017-05-05 18:04:48 +0200

    Add missing injectable in location provider

--- scripts/LocationProvider.ts
+++ scripts/LocationProvider.ts
@@ -1,5 +1,7 @@
 import ILocationProvider from "./ILocationProvider";
+import {injectable} from "inversify";
 
+@injectable()
 class LocationProvider implements ILocationProvider {
 
     getLocation(): Location {
